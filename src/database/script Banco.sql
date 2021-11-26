648d2efd2b1ee4fb2e227d854dbffdb0c5fdf0bf
alvesK23 <GABRIEL.RALVES@bandtec.com.br>
2021-11-25 23:35:52 -0300

    acertando variaveis

--- src/database/script Banco.sql
+++ src/database/script Banco.sql
@@ -1,14 +1,15 @@
+SELECT * FROM db_groupmusic.usuario;
+
 create database Db_Groupmusic;
 use Db_Groupmusic;
 
-create tabable endereco (
+create table endereco (
 idEndereco int primary key,
 bairro varchar(200),
 localidade varchar(200),
 logradouro varchar(200),
 cep int
 );
-
 create table usuario (
 idusuario int primary key auto_increment,
 nome varchar(100),
@@ -16,8 +17,8 @@ apelido varchar(20),
 email varchar(200),
 usuario varchar(200),
 senha varchar(200),
-idade int,
-criacao_conta timestamp default current_timestamp()
+Nascimento date,
+criacao_conta timestamp default current_timestamp(),
 FkIdEndereco int,
 foreign key (FkIdEndereco) references endereco(idEndereco)
 )auto_increment = 1000;
@@ -25,20 +26,19 @@ foreign key (FkIdEndereco) references endereco(idEndereco)
 create table FotoPerfil(
 idfotoPerfil int primary key auto_increment,
 FkIdcadastro int,
-foreign key (FkIdcadastro) references usuario(idusuario)
+foreign key (FkIdcadastro) references usuario(idusuario),
 Link_FtSalva varchar(2000));
-
 
 
 create table preferenciaMusic (
 idpreferenciaMusic int,
 GeneroMusical varchar(200),
-primary key (idpreferenciaMusic , GeneroMusical),
-FkIdusuario int,
-foreign key (FkIdusuario) references usuario(idusuario)
-);
+primary key (idpreferenciaMusic , FkIdusuario),
+FkIdusuario int ,
+foreign key (FkIdusuario) references usuario(idusuario));
 
 create table Musicas (
 FkGeneroMusic int primary key,
-foreign key (FkGeneroMusic) references preferenciaMusic(idpreferenciaMusic)
+foreign key (FkGeneroMusic) references preferenciaMusic(idpreferenciaMusic),
+QtdLike int,
 codMusica varchar(400));
